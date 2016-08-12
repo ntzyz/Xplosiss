17747c79fd16bc27348c57062c27a43ebd90678d
ntzyz <ntzyz@live.cn>
2016-08-12 13:40:37 +0800

    automatic cut post to <!-- more -->

--- api/post.js
+++ api/post.js
@@ -29,16 +29,46 @@ function decodeHTML(str) {
     });
 }
 
+function render(table, part, more) {
+    for (let i = 0; i != table.length; ++i) {
+        if (table[i].render_type == 1) {
+            table[i][part] = pug.render(table[i][part]);
+        } else if (table[i].render_type == 2) {
+            table[i][part] = decodeHTML(marked(table[i][part]));
+        }
+        if (more && table[i][part].indexOf('<!-- more -->') >= 0) {
+            table[i][part] = table[i][part].substr(0, table[i][part].indexOf('<!-- more -->')) 
+        }
+        table[i][part] = table[i][part].replace(/<code lang="(.+?)">([^]+?)<\/code>/g, (match, p1, p2) => {
+            if (table[i].render_type != 2)
+                return '<pre>' + hljs.highlight(p1, p2).value + '</pre>';
+            else 
+                return hljs.highlight(p1, p2).value;
+        }).replace(/<code>([^]+?)<\/code>/g, function(match, p1) {
+            if (table[i].render_type != 2)
+                return '<pre>' + hljs.highlightAuto(p1).value + '</pre>';
+            else
+                return hljs.highlightAuto(p1).value;
+        });
+    }
+    return table;
+}
+
 router.get('/all', (req, res) => {
     utils.getConn().query('select count(*) as cnt from post inner join category where category.category_id = post.post_category_id', (err, table) => {
         if (err)
             throw err;
         let maxPages = Math.ceil(table[0].cnt / 5);
         utils.getConn().query( {
-            sql: 'select post_id, category_name, post_date, post_title, post_shot from post inner join category where category.category_id = post.post_category_id order by post_date desc limit ?',
+            sql: 'select post_id, category_name, post_date, post_title, post_content as post_shot, render_type from post inner join category where category.category_id = post.post_category_id order by post_date desc limit ?',
             values: [((req.query.page + 1) * 5)]
         }, (err, table) => {
-            res.send(JSON.stringify(err ? err : {
+            if (err) {
+                res.send(err);
+                return;
+            }
+            table = render(table, 'post_shot', true);
+            res.send(JSON.stringify({
                 max_pages: maxPages,
                 table: table
             }));
@@ -53,23 +83,9 @@ router.get('/byPostId', (req, res) => {
     }, (err, table) => {
         if (err) {
             res.send(JSON.stringify(err));
+            return;
         }
-        if (table[0].render_type == 1) {
-            table[0].post_content = pug.render(table[0].post_content);
-        } else if (table[0].render_type == 2) {
-            table[0].post_content = decodeHTML(marked(table[0].post_content));
-        }
-        table[0].post_content = table[0].post_content.replace(/<code lang="(.+?)">([^]+?)<\/code>/g, (match, p1, p2) => {
-            if (table[0].render_type != 2)
-                return '<pre>' + hljs.highlight(p1, p2).value + '</pre>';
-            else 
-                return hljs.highlight(p1, p2).value;
-        }).replace(/<code>([^]+?)<\/code>/g, function(match, p1) {
-            if (table[0].render_type != 2)
-                return '<pre>' + hljs.highlightAuto(p1).value + '</pre>';
-            else
-                return hljs.highlightAuto(p1).value;
-        });
+        table = render(table, 'post_content', false);
         res.send(table);
     })
 });
@@ -81,10 +97,15 @@ router.get('/byCategoryId', (req, res) => {
     }, (err, table) => {
         let maxPages = Math.ceil(table[0].cnt / 5);
         utils.getConn().query({
-            sql: 'select post_id, category_name, post_date, post_title, post_shot from post inner join category where category.category_id = post.post_category_id and post.post_category_id = ? order by post_date desc limit ?',
+            sql: 'select post_id, category_name, post_date, post_title, post_content as post_shot from post inner join category where category.category_id = post.post_category_id and post.post_category_id = ? order by post_date desc limit ?',
             values: [req.query.category_id, (req.query.page + 1) * 5]
         }, (err, table) => {
-            res.send(JSON.stringify(err ? err : {
+            if (err) {
+                res.send(err);
+                return;
+            }
+            table = render(table, 'post_shot', true);
+            res.send(JSON.stringify({
                 max_pages: maxPages,
                 table: table
             }));
