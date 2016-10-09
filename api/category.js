fdc7987eec027e313934667a7c28079cfb80d88a
ntzyz <ntzyz@live.cn>
2016-10-09 17:33:26 +0800

    Security update

--- api/category.js
+++ api/category.js
@@ -11,23 +11,4 @@ router.get('/list', (req, res) => {
     });
 });
 
-router.get('/add', (req, res) => {
-    if (!req.query.name) {
-        res.send(JSON.stringify({
-            status: 'ERROR',
-            message: 'Category name not specfied.'
-        }));
-    }
-    else {
-        utils.getConn().query({
-            sql: 'INSERT INTO category(category.category_name) VALUES(?)',
-            values: [req.query.name]
-        }, (err, table) => {
-            res.send(JSON.stringify(err ? err : {
-                'status': 'ok'
-            }));
-        })
-    }
-});
-
 module.exports = router;
