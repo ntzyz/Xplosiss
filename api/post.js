bdb7298769f7a0970851fea84226884fc59e17f3
ntzyz <ntzyz@live.cn>
2016-10-17 03:43:08 +0800

    Sort posts by post date instead of post id.

--- api/post.js
+++ api/post.js
@@ -60,13 +60,20 @@ router.get('/all', (req, res) => {
             throw err;
         let maxPages = Math.ceil(table[0].cnt / 5);
         utils.getConn().query( {
-            sql: 'select post_id, category_name, post_date, post_title, post_content as post_shot, render_type from post inner join category where category.category_id = post.post_category_id order by post_id desc limit ?',
+            sql: 'select post_id, category_name, post_date, post_title, post_content as post_shot, render_type from post inner join category where category.category_id = post.post_category_id',
             values: [((req.query.page + 1) * 5)]
         }, (err, table) => {
             if (err) {
                 res.send(err);
                 return;
             }
+            table = table.map(row => {
+                row['time'] = (new Date(row['post_date'])).getTime()
+                return row;
+            });
+            table.sort((arg1, arg2) => {
+                return arg1['time'] < arg2['time'];
+            });
             table = render(table, 'post_shot', true);
             res.send(JSON.stringify({
                 max_pages: maxPages,
