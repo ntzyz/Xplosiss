438c75bb2273b1d5d8a1e1073bc6b095f0ec43fb
ntzyz <ntzyz@live.cn>
2017-08-13 20:34:27 +0800

    Update configurations.

--- config.sample.js
+++ config.sample.js
@@ -1,6 +1,9 @@
 module.exports = {
   database: 'mongodb://localhost:27017/newBlog?autoReconnect=true',
   port: 1234,
+  page: {
+    size: 5,
+  },
   reply: {
     enableMarkdownSupport: true,
   }
