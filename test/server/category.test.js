e7176ce1c9ce41d56bf11e778f13cf92ca1b0fe5
ntzyz <ntzyz@live.cn>
2018-03-06 01:23:43 +0800

    Fix tests.

--- test/server/category.test.js
+++ test/server/category.test.js
@@ -20,16 +20,22 @@ describe('Testing category-related APIs.', () => {
   });
 
   it('Fetch posts under category', async () => {
-    const url = `/api/category/${categories[3]}/posts`;
+    const url = `/api/category/${categories[0]}/posts`;
     const response = await agent.get(url).expect(200);
     expect(response.body.status).to.be.ok;
     expect(response.body.posts).not.to.be.undefined;
   });
 
   it('Fetch posts under category with page', async () => {
-    const url = `/api/category/${categories[0]}/posts?page=2`;
+    const url = `/api/category/${categories[0]}/posts?page=1`;
     const response = await agent.get(url).expect(200);
     expect(response.body.status).to.be.ok;
     expect(response.body.posts).not.to.be.undefined;
+  });
+
+  it('Fetch posts under category with out-of-ranged page', async () => {
+    const url = `/api/category/${categories[0]}/posts?page=100`;
+    const response = await agent.get(url).expect(404);
+    expect(response.body.status).equals('error');
   });
 });
