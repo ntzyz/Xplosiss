765b51508ce1a767be47ae1fc081a6b43c24a86f
ntzyz <ntzyz@live.cn>
2018-10-02 22:41:43 +0800

    package: add upgrade scripts

---
+++ scripts/upgrade.js
@@ -0,0 +1,69 @@
+const child_process = require('child_process');
+const fs = require('fs');
+const path = require('path');
+
+function getCurrentCommitHash () {
+  return new Promise((resolve) => {
+    child_process.exec('git rev-parse HEAD', (err, stdout) => {
+      resolve(stdout);
+    });
+  });
+}
+
+function evalGitPull () {
+  return new Promise((resolve) => {
+    child_process.exec('git pull', (err, stdout) => {
+      resolve(stdout);
+    });
+  });
+}
+
+function getCommitSinceHash (hash) {
+  return new Promise((resolve) => {
+    child_process.exec(`git rev-list ${hash}..HEAD`, (err, stdout) => {
+      resolve(stdout.split('\n'));
+    });
+  });
+}
+
+function evalBuildBundle () {
+  return new Promise((resolve) => {
+    child_process.exec('npm run build', (err, stdout) => {
+      resolve();
+    });
+  });
+}
+
+async function main () {
+  const currentCommitHash = await getCurrentCommitHash();
+  console.log(`Current HEAD is ${currentCommitHash}`);
+
+  console.log('Fetching the latest code...');
+  await evalGitPull();
+
+  const hashs = [
+    currentCommitHash,
+    ...await getCommitSinceHash(currentCommitHash),
+  ];
+
+  console.log('Checking scripts to run...');
+  for (const hash of hashs) {
+    if (fs.existsSync(path.join(__dirname, `./${hash}.js`))) {
+      try {
+        await require(`./${hash}.js`)();
+      } catch (e) {
+        console.log(`Error on running ${hash}.js: ${e}`);
+        process.exit(1);
+      }
+      console.log(`${hash} => ok!`);
+    }
+  }
+
+  console.log('Building frontend bundles...');
+  await evalBuildBundle();
+
+  console.log('All done.');
+  process.exit(0);
+}
+
+main();
