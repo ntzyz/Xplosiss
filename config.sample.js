34349b79ebab2f57e3d1392a277cde789ccf4996
ntzyz <ntzyz@live.cn>
2019-01-02 11:04:07 +0800

    license: update

--- config.sample.js
+++ config.sample.js
@@ -9,7 +9,7 @@ const config = {
   title: 'ntzyz\'s space',
   subtitle: '∠( ᐛ 」∠)_',
   footer: [
-    'Copyright © 2016-2018 ntzyz. All rights reversed.',
+    'Copyright © 2016-2019 ntzyz. All rights reversed.',
     'Except where otherwise noted, content on this blog is licensed under CC-BY 2.0.'
   ],
   url: 'https://new.ntzyz.cn', // no slash at the end of url
