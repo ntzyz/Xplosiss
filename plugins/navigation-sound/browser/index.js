2b77cb265c107efc9035e56dec06666c424c3dd2
ntzyz <ntzyz@live.cn>
2019-04-17 18:07:55 +0800

    plugin: minor changes

--- plugins/navigation-sound/browser/index.js
+++ plugins/navigation-sound/browser/index.js
@@ -1,16 +1,23 @@
 export function pluginInstaller ({ app, router, store, coreComponents, config }) {
-  if (process.env.VUE_ENV !== 'server') {
-    const audioEffect = new Audio();
-    audioEffect.src = config.plugins['navigation-sound'].audioURL;
-    audioEffect.loop = false;
-  
-    router.beforeResolve((to, from, next) => {
-      try {
-        audioEffect.play();
-      } catch (e) { /* doing nothing here */ }
-      next();
-    });
+  // Disable when server rendering
+  if (process.env.VUE_ENV === 'server') {
+    return;
   }
+
+  // Disable for mobile devices, as playing sound on these devices may
+  // activate the global media control widget/notification
+  if (/(iPhone|IEMobile|Mobile|Android)/.test(navigator.userAgent)) {
+    return;
+  }
+
+  const audioEffect = new Audio();
+  audioEffect.src = config.plugins['navigation-sound'].audioURL;
+  audioEffect.loop = false;
+
+  router.beforeResolve((_to, _from, next) => {
+    audioEffect.play().catch(_error => { /* actually we do NOT care what happened. */ });
+    next();
+  });
 
   return Promise.resolve();
 }
