b909cf8b13874eeaa562005dcde5898292b95570
ntzyz <ntzyz@live.cn>
2019-06-10 10:32:58 +0800

    spa: bug fix

--- src/utils/link-injector.js
+++ src/utils/link-injector.js
@@ -8,7 +8,7 @@ const clickEventMixin = {
       while (target !== null && target.tagName !== 'A') {
         target = target.parentNode;
       }
-      if (target && target.href && target.href.indexOf(window.location.origin) === 0) {
+      if (target && target.href && target.href.indexOf(window.location.origin) === 0 && target.getAttribute('inject') !== 'no') {
         event.preventDefault();
         let url = new window.URL(target.href);
         this.$router.push(url.href.replace(url.origin, ''));
