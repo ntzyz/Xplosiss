3244e9d804d58e0ad34f9c64106a7230aebed268
ntzyz <i@ntzyz.io>
2020-10-07 23:16:41 +0800

    refactor: use separate component for monaco-editor

---
+++ src/components/admin/Monaco.vue
@@ -0,0 +1,176 @@
+<template lang="pug">
+  div.monaco-editor(ref="monacoEditor" @paste="onPasteFile($event)" @drop="onDropFile($event)" @dragover="$event.preventDefault()")
+</template>
+
+<script>
+import api from '../../api';
+
+export default {
+  name: 'Monaco',
+  props: {
+    value: {
+      type: String,
+      default: ''
+    },
+    language: {
+      type: String,
+      default: ''
+    },
+  },
+  data () {
+    return {
+      monaco: null,
+    }
+  },
+  watch: {
+    value (newValue) {
+      if (this.monaco) {
+        if (newValue === this.monaco.getValue()) {
+          return;
+        }
+        this.monaco.setValue(newValue || '');
+      }
+    },
+    language (language) {
+      const formatLUT = {
+        'jade': 'pug',
+        'HTML': 'html',
+        'markdown': 'markdown'
+      };
+      if (this.monaco) {
+        monaco.editor.setModelLanguage(this.monaco.getModel(), formatLUT[language]);
+      }
+    }
+  },
+  mounted () {
+    import('monaco-editor').then(monaco => {
+      this.monaco = monaco.editor.create(this.$refs.monacoEditor, {
+        fontSize: '12px',
+        language: this.$props.language || '',
+        folding: true,
+        foldingStrategy: 'indentation',
+        wordWrap: 'on',
+        automaticLayout: true,
+        overviewRulerBorder: true,
+        scrollBeyondLastLine: true,
+        minimap: {
+          enabled: false
+        },
+        value: this.value
+      });
+
+      this.monaco.onDidBlurEditorText((e) => {
+        this.$emit('input', this.monaco.getValue())
+      })
+    });
+  },
+  beforeDestroy () {
+    if (this.monaco) {
+      this.monaco.dispose();
+    }
+  },
+  methods: {
+    onPasteFile (event) {
+      const pastedItems = [...event.clipboardData.items];
+      console.log(event.clipboardData.files);
+
+      for (const item of pastedItems) {
+        const file = item.getAsFile();
+        this.uploadFile(file, event.target);
+      }
+    },
+    onDropFile (event) {
+      event.preventDefault();
+      event.stopPropagation();
+      const files = [...event.dataTransfer.files];
+
+      files.forEach(file => this.uploadFile(file, event.target));
+    },
+    async uploadFile (file, target) {
+      if (!file) {
+        return;
+      }
+
+      const comment = `<!-- UPLOAD PLACEHOLDER ID ${this.uploadId++} -->`;
+      const editorBackendIsTextArea = !this.monaco;
+      let html = '';
+
+      if (editorBackendIsTextArea) {
+        target.setRangeText(comment);
+      } else {
+        const editor = this.monaco;
+        const selection = editor.getSelection();
+        const model = editor.getModel();
+
+        editor.executeEdits('update-value', [{
+          range: selection,
+          text: comment,
+          forceMoveMarkers: true
+        }]);
+
+        editor.setSelection(new window.monaco.Selection(selection.startLineNumber, selection.startColumn, selection.endLineNumber, selection.endColumn + comment.length));
+      }
+
+      try {
+        const response = await api.media.uploadFile({
+          file,
+          token: this.$store.state.token,
+          convert: /\.(jpg|jpeg|png|webp|bmp)$/.test(file.name),
+        });
+        const filename = response.data.filename;
+
+        if (/\.(jpg|jpeg|png|webp|bmp|svg)$/.test(filename)) {
+          const alt = response.data.alternative;
+          
+          if (alt.jpeg || alt.webp) {
+            const sources = [];
+            
+            alt.webp && sources.push(`  <source srcset="/api/media/${encodeURIComponent(alt.webp)}" type="image/webp">`);
+            alt.jpeg && sources.push(`  <source srcset="/api/media/${encodeURIComponent(alt.jpeg)}" type="image/jpeg">`);
+
+            html = `<picture>\n${sources.join('\n')}\n  <img src="/api/media/${encodeURIComponent(filename)}">\n</picture>`;
+          } else {
+            html = `<img src="/api/media/${encodeURIComponent(filename)}">`;
+          }
+        } else {
+          html = `<a href="/api/media/${encodeURIComponent(filename)}">${file.name}</a>`;
+        }
+      } catch (ex) {
+        console.error(ex);
+      }
+
+      if (editorBackendIsTextArea) {
+        const begin = target.value.indexOf(comment);
+        const end = begin + comment.length;
+        
+        target.setRangeText(html, begin, end);
+        target.setSelectionRange(begin + html.length, begin + html.length);
+
+        const event = document.createEvent('HTMLEvents');
+        event.initEvent('input', false, true);
+
+        this.$refs.textarea.forEach(el => {
+          el.dispatchEvent(event);
+        });
+      } else {
+        const editor = this.monaco;
+        const value = editor.getValue();
+        const position = editor.getModel().getPositionAt(value.indexOf(comment));
+        editor.executeEdits('update-value', [{
+          range: new window.monaco.Selection(position.lineNumber, position.column, position.lineNumber, position.column + comment.length),
+          text: html,
+          forceMoveMarkers: true
+        }]);
+      }
+    }
+
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+div.monaco-editor {
+  border: 1px solid #ccc;
+  height: 600px;
+}
+</style>
--- src/components/admin/PostEditor.vue
+++ src/components/admin/PostEditor.vue
@@ -74,9 +74,10 @@
           td.label 默认语言：
           td
             input(type="checkbox" v-model="currentEditingBody.default")
-      tr
+      tr(v-show="editingLanguage")
         td.label 文章内容：
-        td: div.monaco-inject(ref="monaco" @paste="onPasteFile($event)" @drop="onDropFile($event)" @dragover="$event.preventDefault()")
+        td
+          monaco(v-model="currentEditingBody.content" :language="currentEditingBody.format")
       tr
         td
         td
@@ -89,9 +90,11 @@
 <script>
 import api from '../../api';
 import preventLeaveMixin from '../../mixins/prevent-leave';
+import Monaco from './Monaco.vue';
 
 export default {
   name: 'PostEditor',
+  components: { Monaco },
   mixins: [preventLeaveMixin],
   data () {
     let date = new Date();
@@ -119,48 +122,31 @@ export default {
       body: [],
       editingLanguage: '',
       uploadId: 0,
-      currentEditingBody: {},
-      monaco: null,
     };
   },
   computed: {
     categories () {
       return this.$store.state.categories;
+    },
+    currentEditingBody () {
+      return this.body.filter(el => el.language === this.editingLanguage)[0] || {};
     }
   },
   watch: {
     editingLanguage (val) {
-      if (val) {
-        if (this.monaco) {
-          this.currentEditingBody.content = this.monaco.getValue();
-        }
-
-        this.currentEditingBody = this.body.filter(el => el.language === val)[0];
-
-        if (this.monaco) {
-          this.monaco.setValue(this.currentEditingBody.content);
-        }
+      if (val || val === undefined) {
         return;
       }
+      
       const result = window.prompt('叫啥呢？');
       if (result && result.length > 0) {
         this.body.push({
           content: '',
           format: '',
           language: result,
-          default: false,
+          default: this.body === 0,
         });
         this.editingLanguage = result;
-      }
-    },
-    'currentEditingBody.format': function (val) {
-      const formatLUT = {
-        'jade': 'pug',
-        'HTML': 'html',
-        'markdown': 'markdown'
-      };
-      if (this.monaco) {
-        window.monaco.editor.setModelLanguage(this.monaco.getModel(), formatLUT[val]);
       }
     },
     '$route': function () {
@@ -175,6 +161,8 @@ export default {
       this.tagsSet = new Set();
       if (this.$route.params.id) {
         this.fetchPost(true);
+      } else {
+        this.editingLanguage = undefined;
       }
     }
   },
@@ -189,38 +177,9 @@ export default {
     this.fetchTags();
     this.fetchCategories();
     // document.querySelector('#app').style.maxWidth = 'initial';
-
-    this.$nextTick(() => {
-      import('monaco-editor').then(monaco => {
-        if (window.monacoEditor) {
-          window.monacoEditor.dispose();
-        }
-
-        this.monaco = monaco.editor.create(this.$refs.monaco, {
-          fontSize: '12px',
-          language: this.currentEditingBody.format,
-          folding: true,
-          foldingStrategy: 'indentation',
-          wordWrap: 'on',
-          automaticLayout: true,
-          overviewRulerBorder: true,
-          scrollBeyondLastLine: true,
-          minimap: {
-            enabled: false
-          },
-          value: this.currentEditingBody.content
-        });
-
-        window.monacoEditor = this.monaco; 
-      });
-    });
   },
   beforeDestroy () {
     // document.querySelector('#app').style.maxWidth = '';
-    if (this.monaco) {
-      this.monaco.dispose();
-      window.monacoEditor = null;
-    }
   },
   methods: {
     prompt (string) {
@@ -242,10 +201,6 @@ export default {
       this.tags = [...this.tagsSet];
     },
     updatePost () {
-      if (this.monaco) {
-        this.currentEditingBody.content = this.monaco.getValue();
-      }
-
       api.post.updatePostById({
         token: this.$store.state.token,
         id: this.id,
@@ -274,10 +229,6 @@ export default {
       if (!this.slug) {
         alert('URL 名称是必须的！');
         return;
-      }
-
-      if (this.monaco) {
-        this.currentEditingBody.content = this.monaco.getValue();
       }
 
       api.post.createPost({
@@ -317,19 +268,7 @@ export default {
         this.password = post.password;
         this.body = post.body;
 
-        (!keepSelectedLanguage) && this.$nextTick(() => {
-          try {
-            this.editingLanguage = this.body.filter(body => body.default)[0].language;
-            if (this.monaco) {
-              this.$nextTick(() => {
-                this.monaco.setValue(this.currentEditingBody.content);
-              });
-            }
-          } catch (_) {
-          }
-        });
-
-        let tmpDate = new Date(post.date);
+        const tmpDate = new Date(post.date);
         this.date = {
           year: tmpDate.getFullYear(),
           month: tmpDate.getMonth() + 1,
@@ -338,6 +277,10 @@ export default {
           minute: tmpDate.getMinutes(),
           second: tmpDate.getSeconds(),
         };
+
+        if (!keepSelectedLanguage) {
+          this.editingLanguage = (this.body.filter(body => body.default)[0] || {}).language;
+        }
       });
     },
     fetchTags () {
@@ -356,99 +299,6 @@ export default {
         alert('会话过期，请手动刷新');
       });
     },
-    onPasteFile (event) {
-      const pastedItems = [...event.clipboardData.items];
-      console.log(event.clipboardData.files);
-
-      for (const item of pastedItems) {
-        const file = item.getAsFile();
-        this.uploadFile(file, event.target);
-      }
-    },
-    onDropFile (event) {
-      event.preventDefault();
-      event.stopPropagation();
-      const files = [...event.dataTransfer.files];
-
-      files.forEach(file => this.uploadFile(file, event.target));
-    },
-    async uploadFile (file, target) {
-      if (!file) {
-        return;
-      }
-
-      const comment = `<!-- UPLOAD PLACEHOLDER ID ${this.uploadId++} -->`;
-      const editorBackendIsTextArea = !this.monaco;
-      let html = '';
-
-      if (editorBackendIsTextArea) {
-        target.setRangeText(comment);
-      } else {
-        const editor = this.monaco;
-        const selection = editor.getSelection();
-        const model = editor.getModel();
-
-        editor.executeEdits('update-value', [{
-          range: selection,
-          text: comment,
-          forceMoveMarkers: true
-        }]);
-
-        editor.setSelection(new window.monaco.Selection(selection.startLineNumber, selection.startColumn, selection.endLineNumber, selection.endColumn + comment.length));
-      }
-
-      try {
-        const response = await api.media.uploadFile({
-          file,
-          token: this.$store.state.token,
-          convert: /\.(jpg|jpeg|png|webp|bmp)$/.test(file.name),
-        });
-        const filename = response.data.filename;
-
-        if (/\.(jpg|jpeg|png|webp|bmp|svg)$/.test(filename)) {
-          const alt = response.data.alternative;
-          
-          if (alt.jpeg || alt.webp) {
-            const sources = [];
-            
-            alt.webp && sources.push(`  <source srcset="/api/media/${encodeURIComponent(alt.webp)}" type="image/webp">`);
-            alt.jpeg && sources.push(`  <source srcset="/api/media/${encodeURIComponent(alt.jpeg)}" type="image/jpeg">`);
-
-            html = `<picture>\n${sources.join('\n')}\n  <img src="/api/media/${encodeURIComponent(filename)}">\n</picture>`;
-          } else {
-            html = `<img src="/api/media/${encodeURIComponent(filename)}">`;
-          }
-        } else {
-          html = `<a href="/api/media/${encodeURIComponent(filename)}">${file.name}</a>`;
-        }
-      } catch (ex) {
-        console.error(ex);
-      }
-
-      if (editorBackendIsTextArea) {
-        const begin = target.value.indexOf(comment);
-        const end = begin + comment.length;
-        
-        target.setRangeText(html, begin, end);
-        target.setSelectionRange(begin + html.length, begin + html.length);
-
-        const event = document.createEvent('HTMLEvents');
-        event.initEvent('input', false, true);
-
-        this.$refs.textarea.forEach(el => {
-          el.dispatchEvent(event);
-        });
-      } else {
-        const editor = this.monaco;
-        const value = editor.getValue();
-        const position = editor.getModel().getPositionAt(value.indexOf(comment));
-        editor.executeEdits('update-value', [{
-          range: new window.monaco.Selection(position.lineNumber, position.column, position.lineNumber, position.column + comment.length),
-          text: html,
-          forceMoveMarkers: true
-        }]);
-      }
-    }
   },
 };
 
@@ -467,10 +317,6 @@ div.post-editor {
   button.tag {
     margin-right: 4px;
     margin-bottom: 4px;
-  }
-  div.monaco-inject {
-    border: 1px solid #ccc;
-    height: 600px;
   }
   button {
     font-size: 12px;
